22d54fe6a2ad5865c68be29831102fc7a974d3f3
Robin Davies <robbiedaves@hotmail.com>
2017-04-17 16:59:00 +0100

    Hello world initial commit

---
+++ src/main/java/com/robbiedaves/simplesparkblog/BlogService.java
@@ -0,0 +1,18 @@
+package com.robbiedaves.simplesparkblog;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+
+public class BlogService {
+
+    public static void main( String[] args) {
+        get("/posts", (req, res) -> {
+            return "Hello Sparkingly World!";
+        });
+    }
+
+}
